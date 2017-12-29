567e4a8b01691d29d8dbef8f494fad35ac14736c
ankhleu <summerlightwind@gmail.com>
2017-12-29 09:12:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example1/student/example20171228_03/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example1.student.example20171228_03;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        Intent it=new Intent();
+        it.setAction("dd123");
+        startActivity(it);
+    }
+
+
+}
